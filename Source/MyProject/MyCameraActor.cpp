fa4eb770965cad4cd60c4db0c906b626f4cd3cec
David W.K <dwelchkeliihoomalu@gmail.com>
2023-10-19 19:56:10 -1000

    Refactored camera actor

--- Source/MyProject/MyCameraActor.cpp
+++ Source/MyProject/MyCameraActor.cpp
@@ -1,10 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Kismet/GameplayStatics.h"
-#include "MyPlayerController.h"
-#include "Camera/CameraActor.h"
 #include "MyCameraActor.h"
-
+#include "Kismet/GameplayStatics.h"
 
 
 AMyCameraActor::AMyCameraActor()
@@ -15,16 +12,10 @@ AMyCameraActor::AMyCameraActor()
 void AMyCameraActor::BeginPlay()
 {
 	Super::BeginPlay();
-	//FTransform transform(FRotator{ .0f, -90.0f, .0f }, FVector{ 512.0f, 512.0f, 288.0f }, FVector{ 1.0f, 1.0f, 1.0f });
-	ACameraActor* MyCameraActor = this;
 
+	ACameraActor* MyCameraActor = this;
 	
-	//MyCameraActor->SetActorTransform(transform);
 	APlayerController* MyPlayerController = UGameplayStatics::GetPlayerController(this, 0);
 	MyPlayerController->SetViewTargetWithBlend(MyCameraActor,.0f);
-	//MyPlayerController->GetPawn()->GetComponentByClass<UPaperSpriteComponent>()->SetWorldRotation(FRotator(0.f, 0.f, 0.f));
-	//MyPlayerController->GetPawn()->SetActorRotation(FRotator(0.f, 0.f, 0.f));
-
-	
 }
 
