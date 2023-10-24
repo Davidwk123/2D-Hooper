8cd78684134ebfe83acd7990a51796ce241bf1e8
David W.K <dwelchkeliihoomalu@gmail.com>
2023-10-23 18:04:08 -1000

    Created PauseMenu Widget

---
+++ Source/MyProject/MyPauseMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyPauseMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UMyPauseMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
